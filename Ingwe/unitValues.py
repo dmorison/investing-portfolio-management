8e8ccba11486a5edb63fa1486563768c78220bfe
David Morison <davemorison@gmail.com>
2021-02-09 14:31:38 +0000

    better seperation between portfolios

--- Ingwe/unitValues.py
+++ Ingwe/unitValues.py
@@ -8,51 +8,30 @@ ts_df = transactions[['typeid', 'value', 'symbol']]
 ts_df = ts_df.sort_index()
 ts_df = ts_df.rename_axis('Date')
 
-running_total = 0
+current_balance = 0
 def sum_cash(a, b):
-    global running_total
+    global current_balance
     if a == 'buylong' or a == 'quarterlymanagementfee':
-        running_total = running_total - b
+        current_balance = current_balance - b
     else:
-        running_total = running_total + b
-    return running_total
+        current_balance = current_balance + b
+    return current_balance
 
-ts_df = ts_df.assign(cash_balance = ts_df.apply(lambda x: sum_cash(a = x['typeid'], b = x['value']), axis=1))
-# print(ts_df.head(10))
-df_cash_balance = ts_df[['cash_balance']]
-# print(df_cash_balance)
-# print(df_cash_balance.shape)
+ts_df = ts_df.assign(Cash_balance = ts_df.apply(lambda x: sum_cash(a = x['typeid'], b = x['value']), axis=1))
+df_cash_balance = ts_df[['Cash_balance']]
 
 df = df_cash_balance.groupby(df_cash_balance.index).tail(1)
-print(df.head(10))
-# print(df.shape)
-print(performance.head(10))
 
 df1 = performance.join(df, how='outer')
-print(df1.head(10))
 
-current_cash = 0
-def calculate_cash(a, b):
-    global current_cash
-    profit_value = a
-
-    if pd.isna(a):
-        profit_value = 0
-
-    if pd.notna(b):
-        current_cash = b
-        return profit_value + b
-    else:
-        return profit_value + current_cash
-
-current_balance = 0
+running_balance = 0
 def calculate_running_balance(a):
-    global current_balance
+    global running_balance
     if pd.notna(a):
-        current_balance = a
+        running_balance = a
         return a
     else:
-        return current_balance
+        return running_balance
 
 def calculate_unit_value(a, b):
     profit = 0
@@ -61,9 +40,10 @@ def calculate_unit_value(a, b):
     percentage = profit / b
     return np.round_(1 + percentage, decimals=4)
 
-# df1 = df1.assign(Balance = df1.apply(lambda x: calculate_cash(a = x['Total_profit'], b = x['cash_balance']), axis=1))
-df1 = df1.assign(Running_balance = df1['cash_balance'].map(calculate_running_balance))
+df1 = df1.assign(Running_balance = df1['Cash_balance'].map(calculate_running_balance))
 df1 = df1.assign(Net_asset_value = df1['Total_cost'].fillna(0) + df1['Total_profit'].fillna(0) + df1['Running_balance'])
 df1 = df1.assign(Unit_value = df1.apply(lambda x: calculate_unit_value(a = x['Total_profit'], b = x['Net_asset_value']), axis=1))
 df1 = df1.assign(Unit_val_change = df1['Unit_value'].map(lambda x: np.round_(((x - 1) / 1) * 100, decimals=2)))
 print(df1)
+
+df1.to_csv('daily_unit_value.csv', encoding='utf-8')
