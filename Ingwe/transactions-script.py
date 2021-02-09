d31a0856a9b2a44a50f1b7253c1bd8b62a8d4924
David Morison <davemorison@gmail.com>
2021-02-09 13:54:02 +0000

    calculating Ingwe NAV and unit values

--- Ingwe/transactions-script.py
+++ Ingwe/transactions-script.py
@@ -18,16 +18,52 @@ def sum_cash(a, b):
     return running_total
 
 ts_df = ts_df.assign(cash_balance = ts_df.apply(lambda x: sum_cash(a = x['typeid'], b = x['value']), axis=1))
-
+# print(ts_df.head(10))
 df_cash_balance = ts_df[['cash_balance']]
 # print(df_cash_balance)
 # print(df_cash_balance.shape)
 
-df1 = df_cash_balance.groupby(df_cash_balance.index).tail(1)
-# print(df1)
-# print(df1.shape)
+df = df_cash_balance.groupby(df_cash_balance.index).tail(1)
+print(df.head(10))
+# print(df.shape)
+print(performance.head(10))
 
-df = ts_df.join(performance, how='outer')
-print(df)
-df = df.join(df1, how='outer', rsuffix="_right")
-print(df)
+df1 = performance.join(df, how='outer')
+print(df1.head(10))
+
+current_cash = 0
+def calculate_cash(a, b):
+    global current_cash
+    profit_value = a
+
+    if pd.isna(a):
+        profit_value = 0
+
+    if pd.notna(b):
+        current_cash = b
+        return profit_value + b
+    else:
+        return profit_value + current_cash
+
+current_balance = 0
+def calculate_running_balance(a):
+    global current_balance
+    if pd.notna(a):
+        current_balance = a
+        return a
+    else:
+        return current_balance
+
+def calculate_unit_value(a, b):
+    profit = 0
+    if pd.notna(a):
+        profit = a
+    percentage = profit / b
+    return np.round_(1 + percentage, decimals=4)
+
+# df1 = df1.assign(Balance = df1.apply(lambda x: calculate_cash(a = x['Total_profit'], b = x['cash_balance']), axis=1))
+df1 = df1.assign(Running_balance = df1['cash_balance'].map(calculate_running_balance))
+df1 = df1.assign(Net_asset_value = df1['Total_cost'].fillna(0) + df1['Total_profit'].fillna(0) + df1['Running_balance'])
+df1 = df1.assign(Unit_value = df1.apply(lambda x: calculate_unit_value(a = x['Total_profit'], b = x['Net_asset_value']), axis=1))
+df1 = df1.assign(Unit_val_change = df1['Unit_value'].map(lambda x: np.round_(((x - 1) / 1) * 100, decimals=2)))
+print(df1)
