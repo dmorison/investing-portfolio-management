fcf1164342370f4a9f593cd813066b63c342a01a
David Morison <davemorison@gmail.com>
2021-02-13 12:41:25 +0000

    first chart attempt and finding blank values in calculations for US stocks on bank holidays because of exchange rates still existing

---
+++ chartScript.py
@@ -0,0 +1,28 @@
+#%%
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+# mpl.use('agg')
+import numpy as np
+
+# import seaborn as sns
+# sns.set(rc={'figure.figsize':(11, 4)})
+
+daily_units = pd.read_csv("./Ingwe/daily_unit_value.csv", index_col=0, parse_dates=True)
+# print(daily_units.head(3))
+# print(daily_units.tail(3))
+# print(daily_units.dtypes)
+# print(daily_units.index)
+print(daily_units.loc['2020-10-29'])
+
+daily_units['Weekday'] = daily_units.index.weekday_name
+# print(daily_units.head())
+
+daily_units['Performance'].plot()
+plt.show()
+# %%
+print(daily_units['Performance'].describe())
+maxpf = daily_units['Performance'].idxmax()
+print(daily_units.loc[maxpf])
+
+# %%
