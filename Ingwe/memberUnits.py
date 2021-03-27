8b9cb8d346d97258a914d9849b8aae3999832a06
David Morison <davemorison@gmail.com>
2021-03-27 14:34:13 +0000

    printing out data studio variables to update at the end

--- Ingwe/memberUnits.py
+++ Ingwe/memberUnits.py
@@ -14,11 +14,11 @@ calc_cash_balance = unit_values['Cash_balance'].iloc[-1]
 calc_investment_value = unit_values['Investment_value'].iloc[-1]
 calc_nav = unit_values['NAV'].iloc[-1]
 
-ttt_date = pd.to_datetime("2021-03-12")
-ttt_unit_val = 1.3212
+ttt_date = pd.to_datetime("2021-03-27")
+ttt_unit_val = 1.2844
 ttt_total_units = 19353.22
-ttt_cash_balance = 1516.62
-ttt_investment_value = 24052.43
+ttt_cash_balance = 1521.88
+ttt_investment_value = 23336.04
 ttt_nav = ttt_cash_balance + ttt_investment_value
 
 unit_values = unit_values[['unit_val']]
@@ -81,4 +81,11 @@ ttt_vs_calc_data = {'ttt': ttt_values,
 ttt_vs_calc_df = pd.DataFrame(ttt_vs_calc_data, index = indx_col)
 ttt_vs_calc_df = ttt_vs_calc_df.assign(Difference = ((ttt_vs_calc_df['calc'] - ttt_vs_calc_df['ttt']) / ttt_vs_calc_df['ttt']) * 100)
 print(ttt_vs_calc_df) #PRINT------------PRINT--------------PRINT#
+
+# get last weeks market value to print out for updating dashboard variable
+last_week = pd.read_csv('./portfolio_performance/ttt_vs_calculations.csv')
+last_week_market_value = last_week.iloc[3,1]
+print('Previous weeks market value: ' + str(last_week_market_value))
+print('Current unit value: ' + str(calc_unit_val))
+
 ttt_vs_calc_df.to_csv('./portfolio_performance/ttt_vs_calculations.csv', float_format='%.2f', encoding='utf-8')
