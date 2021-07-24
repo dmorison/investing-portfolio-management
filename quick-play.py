de563c227a49e3dc3e5f56213cfddb7d7258a985
David Morison <davemorison@gmail.com>
2021-07-24 10:59:42 +0100

    getting 403 trying to request yahoo finance api directly

--- quick-play.py
+++ quick-play.py
@@ -1,11 +1,19 @@
 import pandas as pd
 import numpy as np
 # import yfinance as yf
+import requests
+from bs4 import BeautifulSoup
 
-dv_df = pd.read_csv('./Dave/portfolio_performance/summary_stock_performance_yields.csv', index_col='Date', parse_dates=True)
-types_df = pd.read_csv('./Dave/input_data/stock_types.csv')
-df = pd.merge(dv_df, types_df, how="outer", on="Ticker")
-print(df.info())
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36'}
+url = 'https://uk.finance.yahoo.com/quote/MONY.L'
+stock_url = 'https://query2.finance.yahoo.com/v10/finance/download/MONY.L?period1=1625097600&period2=1626912000&interval=1d&events=history&includeAdjustedClose=true'
+response = requests.get(stock_url)
+print(response.status_code)
+
+# dv_df = pd.read_csv('./Dave/portfolio_performance/summary_stock_performance_yields.csv', index_col='Date', parse_dates=True)
+# types_df = pd.read_csv('./Dave/input_data/stock_types.csv')
+# df = pd.merge(dv_df, types_df, how="outer", on="Ticker")
+# print(df.info())
 
 
 # summary_df = pd.read_csv('./Ingwe/portfolio_performance/summary_stock_performance_yields-original.csv', index_col='Date', parse_dates=True)
